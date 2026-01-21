b624a615d60fd1a17f997f4a96eea027b4b2816c
Larihooo <eliasfabisantos@gmail.com>
2026-01-20 22:10:46 -0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,11 +1,7 @@
-// firebase.js
-// Esse arquivo é o CENTRO DE COMANDO. Ele conecta todos os outros ao Firebase.
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
 
-// SUAS CREDENCIAIS REAIS
 const firebaseConfig = {
   apiKey: "AIzaSyByiAUMQ3I94RkRbjSDF_faBtJdTWfF6Ec",
   authDomain: "siteelievn.firebaseapp.com",
@@ -16,7 +12,6 @@ const firebaseConfig = {
   measurementId: "G-DYL3VDW9S2"
 };
 
-// Inicializa e EXPORTA as ferramentas para os outros arquivos usarem
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
